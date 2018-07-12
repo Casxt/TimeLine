ea96953b79899af0312b2a0f4b56c45cb41498e8
casxt <774714620@qq.com>
2018-07-12 15:12:25 +0800

    delete something useless

--- mail/mail.go
+++ mail/mail.go
@@ -22,7 +22,7 @@ func Sender() (sender gomail.SendCloser, err error) {
 	return Dialer().Dial()
 }
 
-//SendMail sending an email with attchment, it just a little faster and shorter but its calling is not graceful enough
+//SendMail sending an email with attchment, it just a little shorter but its calling is not graceful enough
 //注意区别，sender 在 attachment之前
 func SendMail(To string, Subject string, Content string, sender gomail.SendCloser, Attachments ...Attachment) (err error) {
 	m := gomail.NewMessage()
@@ -64,52 +64,3 @@ func SendMailWithAttch(To string, Subject string, Content string, Attachments []
 	}
 	return gomail.Send(sender, m)
 }
-
-/*
-
-var mailCh chan *gomail.Message
-
-//Start initial the sender
-func Start() {
-	mailCh = make(chan *gomail.Message)
-	go mailSender()
-}
-
-func mailSender() {
-	d := gomail.NewDialer("smtp.qq.com", 587, "774714620@qq.com", "blwcpdcjpxftbdji")
-	var sender gomail.SendCloser
-	var err error
-
-	defer close(mailCh)
-	defer sender.Close() //这里是否要关闭？
-
-	open := false
-	for {
-		select {
-		case mail, ok := <-mailCh:
-			if !ok {
-				log.Println("Mail Server Closed")
-				return
-			}
-			if !open {
-				if sender, err = d.Dial(); err != nil {
-					log.Fatalln(err)
-				}
-				open = true
-			}
-			if err := gomail.Send(sender, mail); err != nil {
-				log.Fatalln(err)
-			}
-		// Close the connection to the SMTP server if no email was sent in
-		// the last 30 seconds.
-		case <-time.After(30 * time.Second):
-			if open {
-				if err := sender.Close(); err != nil {
-					log.Fatalln(err)
-				}
-				open = false
-			}
-		}
-	}
-}
-*/
