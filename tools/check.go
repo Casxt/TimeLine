92fb7e7e556ae49a065002aab8c27b8ca8c61396
casxt <774714620@qq.com>
2018-07-23 22:48:26 +0800

    添加注释

--- tools/check.go
+++ tools/check.go
@@ -2,10 +2,12 @@ package tools
 
 import "regexp"
 
+//CheckImgHash Check ImgHash Format
 func CheckImgHash(Hash string) bool {
 	return regexp.MustCompile("^[a-z0-9]{64}$").MatchString(Hash)
 }
 
+//ChecNickName Check NickName Format
 func ChecNickName(Hash string) bool {
 	return regexp.MustCompile("^[\\S]{2,32}$").MatchString(Hash)
 }
